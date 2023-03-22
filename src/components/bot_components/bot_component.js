99c928a920dd335a8fe96187303516fd5ae20a50
doopel <doopel@protonmail.com>
2023-03-22 11:10:23 +0100

    ads

--- src/components/bot_components/bot_component.js
+++ src/components/bot_components/bot_component.js
@@ -9,12 +9,7 @@ import { Navigate } from 'react-router-dom';
 class Bot extends Component {
   constructor(props) {
     super(props);
-    this.onChangeTaskStatus = this.onChangeTaskStatus.bind(this);
-    this.onChangeLastBeat = this.onChangeLastBeat.bind(this);
-    this.onChangeTaskEndTime = this.onChangeTaskEndTime.bind(this);
-    this.onChangeBotName = this.onChangeBotName.bind(this);
-    this.onChangePlatform = this.onChangePlatform.bind(this);
-    this.onChangeTaskPayload = this.onChangeTaskPayload.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
 
     this.getBot = this.getBot.bind(this);
     this.updateContent= this.updateContent.bind(this);
@@ -48,62 +43,16 @@ class Bot extends Component {
       },
     }));
   }
-
-  onChangeBotName(e) {
-    const bot_name = e.target.value;
-
-    this.setState((prevState) => ({
-        currentBot: {
-        ...prevState.currentBot,
-        bot_name: bot_name,
-      },
-    }));
-  }
-
-  onChangeTaskStatus(e) {
-    const task_status = e.target.value;
+  onInputChange(event) {
+    const { name, value } = event.target;
 
     this.setState((prevState) => ({
-        currentBot: {
-        ...prevState.currentBot,
-        task_status: task_status,
-      },
+      currentBot: {
+      ...prevState.currentBot,
+      [name]: value 
+    },
     }));
-  }
-
-  onChangeLastBeat(e) {
-    const last_beat_time = e.target.value;
-
-    this.setState((prevState) => ({
-        currentBot: {
-        ...prevState.currentBot,
-        last_beat_time: last_beat_time,
-      },
-    }));
-  }
-
-
-  onChangeTaskEndTime(e) {
-    const task_end_time = e.target.value;
-
-    this.setState((prevState) => ({
-        currentBot: {
-        ...prevState.currentBot,
-        task_end_time: task_end_time,
-      },
-    }));
-  }
-
-  onChangePlatform(e) {
-    const platform = e.target.value;
-
-    this.setState((prevState) => ({
-        currentBot: {
-        ...prevState.currentBot,
-        platform: platform,
-      },
-    }));
-  }
+  };
 
   getBot(id) {
     BotDataService.get(id)
@@ -123,8 +72,8 @@ class Bot extends Component {
     .updateBot(this.state.currentBot.id, this.state.currentBot)
     .then((reponse) => {
       console.log(reponse);
-      
       this.setState({ message: "The bot was updated successfully!" });
+      
     })
     .catch((e) => {
       console.log(e);
@@ -150,6 +99,10 @@ class Bot extends Component {
       return <Navigate to="/login" />;
     }
 
+    if(this.state.message === "The bot was updated successfully!"){
+      return <Navigate to="/bots" />;
+    }
+
     return (
       <div>
         {currentBot ? (
@@ -159,7 +112,7 @@ class Bot extends Component {
             <Form.Control type="text" placeholder={currentBot.bot_name} id="title"
                   name="bot_name"
                   value={currentBot.bot_name}
-                  onChange={this.onChangeBotName}/>
+                  onChange={this.onInputChange}/>
             <Form.Text className="text-muted" id="bot_name" value={currentBot.bot_name}>
             {currentBot.bot_name}
             </Form.Text>
@@ -172,7 +125,14 @@ class Bot extends Component {
                 </div>
               </div>
             )}
-            
+          <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={this.updateContent}
+              >
+                Update1
+              </button>
+
           <Button variant="primary" type="submit" onClick={this.updateContent}>
             Update
           </Button>
