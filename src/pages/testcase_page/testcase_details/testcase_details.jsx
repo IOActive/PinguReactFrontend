da21fa6642da9393b93da3ae2a8599f7eb49dc23
doopel <doopel@protonmail.com>
2024-01-15 15:39:46 +0100

    tesetcase download button

--- src/pages/testcase_page/testcase_details/testcase_details.jsx
+++ src/pages/testcase_page/testcase_details/testcase_details.jsx
@@ -9,10 +9,11 @@ import { TestCase } from "../../../models/TestCase"
 import { InformationTable } from "../../../components/InformationTable/InformationTable";
 import { faBug } from "@fortawesome/free-solid-svg-icons";
 import { retrieveTestCaseCrashes } from "../../../actions/crash"
-import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Breadcrumb, BreadcrumbItem, Button, ButtonGroup } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import InteractiveTable from "../../../components/Interactive_List/InteractiveList";
-import { Crash } from "../../../models/Crash"
+import { Crash } from "../../../models/Crash";
+import { Buffer } from 'buffer';
 
 const TestCaseDetails = (props) => {
 
@@ -36,6 +37,26 @@ const TestCaseDetails = (props) => {
         }
     }, [payload])
 
+    function DownloadTestCase() {
+        let testcase_stream_b64 = currentTestcase["test_case"].value;
+        if (testcase_stream_b64 != null) {
+            const urlDecodedBase64 = decodeURIComponent(testcase_stream_b64);
+
+            const buffer = Buffer.from(urlDecodedBase64, 'base64');
+
+            // Create a Blob object from the buffer
+            const blob = new Blob([buffer], { type: 'application/text' });
+
+            // Create a download link for the Blob object and simulate a click on the link to download the file
+            const link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            const split = currentTestcase["absolute_path"].value.split('/');
+            const filename = split[split.length-1];
+            link.download = filename;
+            link.click();
+        }
+    }
+
     return <div className={s.root}>
         <Breadcrumb>
             <BreadcrumbItem>YOU ARE HERE</BreadcrumbItem>
@@ -47,11 +68,19 @@ const TestCaseDetails = (props) => {
             currentTestcase ? (
                 <div responsive className={cx("mb-0", s.CardsGroup)}>
                     <div className={cx(s.CardRow)}>
+                        <div class={cx(s.CardCol)}>
                             <InformationTable
                                 id="Test Case Info Table"
                                 object={currentTestcase}
                                 objectName={"TestCase"}
                             />
+                            <ButtonGroup className={cx(s.CardButtonGroup)}>
+
+                                <Button className={cx(s.DownloadTestCaseButton)} onClick={DownloadTestCase}>
+                                    Download Testcase
+                                </Button>
+                            </ButtonGroup>
+                        </div>
                     </div>
                     <div className={cx(s.CardRow)}>
                         <InteractiveTable
